412a3540b1ac6e5984e1b6b999d3ce2d7149a092
Davide Bersani <davide.bersani96@gmail.com>
2019-06-20 10:53:28 +0200

    Edited Controller of component A.

--- project/a-service/src/main/java/asw/project/a/rest/AController.java
+++ project/a-service/src/main/java/asw/project/a/rest/AController.java
@@ -3,6 +3,7 @@ package asw.project.a.rest;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,16 +16,19 @@ public class AController {
 	@Autowired 
 	private AService aService;
 
+
 	private final Logger logger = Logger.getLogger(AController.class.toString()); 
 	
 	
 	@PostMapping("/")
 	public void insertAnimal() {
+		logger.info("Inserisco nuovo animale.");
 		aService.insertAnimal();
 	}
 	
 	@GetMapping("/")
 	public String getAnimals() {
+		logger.info("Richiedo animali.");
 		return aService.getAnimals();
 	}
 	
