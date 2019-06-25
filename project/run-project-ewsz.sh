edef2b3aba247acd86cab43a28177e7d5eccea74
Davide Bersani <davide.bersani96@gmail.com>
2019-06-25 22:21:44 +0200

    FIxed Docker compose

--- project/run-project-ewsz.sh
+++ project/run-project-ewsz.sh
@@ -4,5 +4,10 @@
 echo Avviare prima dello script start-containers.sh
 
 echo Building Java projects  
-
 gradle build
+
+echo Building docker images
+docker-compose build
+
+echo Launching containers
+docker-compose up
