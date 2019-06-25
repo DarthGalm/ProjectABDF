8d1851e0b77a19fb9e6342db51ee39683fe53f3a
Davide Bersani <davide.bersani96@gmail.com>
2019-06-25 15:04:21 +0200

    Update message

--- project/b-service/src/main/java/asw/project/b/domain/BConsumerService.java
+++ project/b-service/src/main/java/asw/project/b/domain/BConsumerService.java
@@ -3,20 +3,22 @@ package asw.project.b.domain;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
-import asw.project.b.cservice.CClient;
 
 @Service
 public class BConsumerService {
 
 	private Logger logger = Logger.getLogger(BConsumerService.class.toString());
 	
+	@Value("${instanceName}") 
+	private String instanceName;
+	
 	@Autowired
 	private CService cService;
 	
 	public void onMessage(String message) {
 		logger.info("RICEVUTO MESSAGGIO: " + message);
-		cService.saveAnimal(message);
+		cService.saveAnimal(instanceName + ": " + message);
 	}
 }
